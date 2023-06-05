f5b5c8ff1553c455a697979c08bc42ca444b5bd3
Jakub Moszyński <lolin515@wp.pl>
2023-06-05 09:23:41 +0200

    some changes

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -9,6 +9,7 @@
     @else
         <title>Kalendarz</title>
     @endif
+    <link rel="icon" type="image/png" href="{{asset('logo.png')}}" />
     <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
@@ -30,7 +31,7 @@
 @endif
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="{{URL::to('')}}">INDEX</a>
+            <a class="navbar-brand" href="{{URL::to('')}}"><img src="{{asset('logo.png')}}" style="height:50px" alt=""></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
             </button>
